9e041d02c18480d391ab8ac0cd822aa992c52dbc
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2023-01-07 19:06:54 -0300

    apagando sobra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
-import { IrradiacaoService } from './components/irradiancia/irradiacao.service';
+import { IrradiacaoService } from './components/formulario/services/irradiacao.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http"
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HistoricoDeConsumoComponent } from './components/historico-de-consumo/historico-de-consumo.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LocalizacaoComponent } from './components/localizacao/localizacao.component';
-import { LocalizacaoService } from './components/localizacao/localizacao.service';
-import { IrradianciaComponent } from './components/irradiancia/irradiancia.component';
+import { LocalizacaoService } from './components/formulario/services/localizacao-service.service';
 import { ConsumoMedioComponent } from './components/painel/consumo-medio/consumo-medio.component';
 import { IrradiacaoMediaDiariaComponent } from './components/painel/irradiacao-media-diaria/irradiacao-media-diaria.component';
 import { LocalidadeComponent } from './components/painel/localidade/localidade.component';
@@ -18,9 +15,6 @@ import { FormularioComponent } from './components/formulario/formulario.componen
   declarations: [
     AppComponent,
     HeaderComponent,
-    HistoricoDeConsumoComponent,
-    LocalizacaoComponent,
-    IrradianciaComponent,
     ConsumoMedioComponent,
     IrradiacaoMediaDiariaComponent,
     LocalidadeComponent,
--- src/app/components/historico-de-consumo/form-historico.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FormHistorico } from './form-historico';
-
-describe('FormHistorico', () => {
-  it('should create an instance', () => {
-    expect(new FormHistorico()).toBeTruthy();
-  });
-});
